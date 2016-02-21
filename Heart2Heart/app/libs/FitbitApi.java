74a8a2f5da6c155c001d04d297cbe5f04f4cbede
U-I\Ivor <ivorh97@gmail.com>
2016-02-21 00:57:37 -0500

    oauth class

---
+++ Heart2Heart/app/libs/FitbitApi.java
@@ -0,0 +1,23 @@
+public class FitbitApi extends DefaultApi10a
+{
+  private static final String AUTHORIZE_URL = "https://www.fitbit.com/oauth/authorize?token=%s";
+
+  @Override
+  public String getAccessTokenEndpoint()
+  {
+    return "https://api.fitbit.com/oauth/access_token";
+  }
+
+  @Override
+  public String getRequestTokenEndpoint()
+  {
+    return "https://api.fitbit.com/oauth/request_token";
+  }
+
+  @Override
+  public String getAuthorizationUrl(Token requestToken)
+  {
+    return String.format(AUTHORIZE_URL, requestToken.getToken());
+  }
+
+}
